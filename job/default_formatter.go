8e4f24bd0f44c83939868f7df1c4c0fec8935ff0
opensnail <598092184@qq.com>
2024-11-24 23:17:30 +0800

    优化日志格式

--- job/default_formatter.go
+++ job/default_formatter.go
@@ -31,7 +31,7 @@ func (f *DefaultFormatter) Format(entry *logrus.Entry) ([]byte, error) {
 
 	if entry.HasCaller() {
 		// 自定义格式：时间、级别、调用者、消息
-		log := fmt.Sprintf("[%s] [%s] [%s] [%s] [%s:%d] %s\n",
+		log := fmt.Sprintf("[%s] [%s] [%s] [%s] [%s:%d] - %s\n",
 			entry.Time.Format("2006-01-02 15:04:05.000"),
 			d["logger"],
 			strings.ToUpper(entry.Level.String()),
@@ -44,7 +44,7 @@ func (f *DefaultFormatter) Format(entry *logrus.Entry) ([]byte, error) {
 		return []byte(log), nil
 	} else {
 		// 自定义格式：时间、级别、调用者、消息
-		log := fmt.Sprintf("[%s] [%s] [%s] %s\n",
+		log := fmt.Sprintf("[%s] [%s] [%s] \n%s\n",
 			entry.Time.Format("2006-01-02 15:04:05.000"),
 			d["logger"],
 			strings.ToUpper(entry.Level.String()),
